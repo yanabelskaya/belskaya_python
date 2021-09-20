c98d05650255154533f000a919f65e2c571058e9
yanabelskaya <yana-belskaya@inbox.ru>
2021-09-20 11:09:37 +0300

    add a check of the entered value in the task 3

--- task_1.py
+++ task_1.py
@@ -3,7 +3,7 @@ def say_hello_if_more_then_7(number: int) -> str:
         return "Привет"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     number_to_check = input("Please, enter a number: ")
     if number_to_check.isdigit():
         print(say_hello_if_more_then_7(int(number_to_check)))
--- task_3.py
+++ task_3.py
@@ -2,6 +2,18 @@ def print_elements_multiples_of_three(elements_list: list) -> list:
     return [i for i in elements_list if i % 3 == 0]
 
 
-if __name__ == '__main__':
-    list_to_check = map(int, input("Please, input some numbers by space and click enter: ").split(" "))
-    print(print_elements_multiples_of_three(list(list_to_check)))
+def get_number_from_user():
+    list_to_check = []
+    while True:
+        user_input = input("Enter a number, enter 'q' to exit: ")
+        if user_input == "q":
+            break
+        try:
+            list_to_check.append(int(user_input))
+        except ValueError:
+            print("It is not a number, try again!")
+    return list_to_check
+
+
+if __name__ == "__main__":
+    print(print_elements_multiples_of_three(list(get_number_from_user())))
